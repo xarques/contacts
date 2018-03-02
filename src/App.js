3e2589dcfcfb068a0d69c2265cc3ca5a3d91607b
Xavier Arques <xavierarques@yahoo.com>
2018-03-02 11:03:02 +0100

    Add removeContact method

--- src/App.js
+++ src/App.js
@@ -25,10 +25,17 @@ class App extends Component {
       }
     ]
   }
+
+  removeContact = (contact) => {
+    this.setState((state) => ({
+      contacts : state.contacts.filter(c => c.id !== contact.id)
+    }));
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={this.state.contacts}/>
+        <ListContacts onRemoveContact={this.removeContact} contacts={this.state.contacts}/>
       </div>
     )
   }
